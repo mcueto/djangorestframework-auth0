b39082a425d45d428a940255304d92bee32cf5cb
mcueto <cueto@live.cl>
2016-10-08 09:36:44 -0300

    updated version for pypi

--- rest_framework_auth0/__init__.py
+++ rest_framework_auth0/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'djangorestframework-auth0'
-__version__ = '0.0.1'
+__version__ = '0.1.5'
 __author__ = 'Marcelo Cueto'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2015 Marcelo Cueto'
+__copyright__ = 'Copyright 2015-2016 Marcelo Cueto'
 
 # Version synonym
 VERSION = __version__
