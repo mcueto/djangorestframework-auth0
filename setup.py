624e449e88336c063c40d2eab3b48483ec17ef40
mcueto <cueto@live.cl>
2016-10-08 09:55:58 -0300

    changed version in setup.cfg

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'rest_framework_auth0',
   packages = ['rest_framework_auth0'],
-  version = '0.1.1',
+  version = '0.1.5',
   description = 'Django Rest Framework Library to use Auth0 authentication',
   author = 'Marcelo Cueto',
   author_email = 'yo@marcelocueto.cl',
   url = 'https://github.com/mcueto/djangorestframework-auth0',
-  download_url = 'https://github.com/mcueto/djangorestframework-auth0/tarball/0.1',
+  download_url = 'https://github.com/mcueto/djangorestframework-auth0/tarball/0.1.5',
   keywords = ['auth0', 'rest framework', 'django'],
   classifiers=[
       'Environment :: Web Environment',
