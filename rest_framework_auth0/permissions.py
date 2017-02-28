71e48bb77fb1ec84215f3de77fc7f46c09e1dfd1
Snake575 <sarang575@gmail.com>
2017-02-28 02:40:35 -0300

    Removed get_jwt_value added get_payload and made perms inherit from auth

--- rest_framework_auth0/permissions.py
+++ rest_framework_auth0/permissions.py
@@ -1,12 +1,11 @@
 from __future__ import unicode_literals
 
 from rest_framework.permissions import BasePermission
+from rest_framework_auth0.authentication import Auth0JSONWebTokenAuthentication
+from rest_framework_auth0.utils import validate_role_from_payload
 
-from rest_framework_auth0.authentication import jwt_decode_handler
-from rest_framework_auth0.utils import get_jwt_value, validate_role_from_payload
 
-
-class HasRoleBasePermission(BasePermission):
+class HasRoleBasePermission(BasePermission, Auth0JSONWebTokenAuthentication):
     """
     Allows access only to users that have an specific role in their app_metadata
     attribute, app_metadata scope required
@@ -31,24 +30,20 @@ class HasRoleBasePermission(BasePermission):
     }
     """
 
-    role_name = ""
+    role_name = ''
 
     def get_role_name(self):
         return self.role_name
 
     def has_permission(self, request, view):
-
         if request.method == 'OPTIONS':
             return True
 
-        jwt = get_jwt_value(request)
+        payload = self.get_payload(request)
 
         try:
-            payload = jwt_decode_handler(jwt)
-
             return validate_role_from_payload(payload, self.get_role_name())
-
-        except Exception as e:
+        except Exception:
             return False
 
 
