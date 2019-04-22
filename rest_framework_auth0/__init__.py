f9dc210d7192b23644c6343c4dec296f56feb2eb
mcueto <cueto@live.cl>
2019-04-22 04:00:07 -0400

    Update version to 0.5.0

--- rest_framework_auth0/__init__.py
+++ rest_framework_auth0/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'djangorestframework-auth0'
-__version__ = '0.4.6'
+__version__ = '0.5.0'
 __author__ = 'Marcelo Cueto'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2015-2017 Marcelo Cueto'
+__copyright__ = 'Copyright 2015-2019 Marcelo Cueto'
 
 # Version synonym
 VERSION = __version__
