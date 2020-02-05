d676b4e7be0bb080d90706d9a7bd69e2adef9a1f
Marcelo Cueto <cueto@live.cl>
2020-02-05 15:39:59 -0300

    Remove unused default settings #50

--- rest_framework_auth0/settings.py
+++ rest_framework_auth0/settings.py
@@ -14,15 +14,6 @@ DEFAULTS = {
     'JWT_PAYLOAD_GET_USERNAME_HANDLER':
     'rest_framework_auth0.utils.auth0_get_username_from_payload_handler',
     # Copied from rest_framework_jwt settings, temporal while migrating to pyjwt
-    'JWT_SECRET_KEY': settings.SECRET_KEY,
-    'JWT_GET_USER_SECRET_KEY': None,
-    'JWT_VERIFY_EXPIRATION': True,
-    'JWT_PUBLIC_KEY': None,
-    'JWT_VERIFY': True,
-    'JWT_LEEWAY': 0,
-    'JWT_AUDIENCE': None,
-    'JWT_ISSUER': None,
-    'JWT_ALGORITHM': 'HS256',
     'JWT_AUTH_COOKIE': None,
 }
 
